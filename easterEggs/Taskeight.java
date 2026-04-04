316f6ddafa7515e11498888614cb8c09cd1ad2be
Olasunkanmi24 <ayodejisodunke23@gmail.com>
2026-04-04 09:15:27 +0100

    Easter egg

---
+++ easterEggs/Taskeight.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+    public class Taskeight{
+
+          public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+            System.out.print("enter a x: ");
+        int y = input.nextInt();
+            
+            int cube = y * y * y;
+        System.out.printf("square of %d%n", cube);        
+
+
+
+}
+
+
+}

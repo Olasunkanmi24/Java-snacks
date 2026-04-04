729939ae1022c8807abc489b71408fa28835abdb
Olasunkanmi24 <ayodejisodunke23@gmail.com>
2026-04-04 20:34:21 +0100

    updated task

---
+++ easterEggs/meatPie/TaskTen.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+    public class TaskTen{
+
+        public static void main(String[] args){
+
+            Scanner input = new Scanner(System.in);
+             System.out.print("Enter a number: ");
+            int number = input.nextInt();
+            int doubleNumber = number*2;
+
+              System.out.printf("double the number %d%n", doubleNumber);
+
+
+}
+
+}

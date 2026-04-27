c280ca68517ee703c67a48c244e8e0eddc9ab5c4
Olasunkanmi24 <ayodejisodunke23@gmail.com>
2026-04-27 07:17:01 +0100

    update

---
+++ wahala/BackToSender.java
@@ -0,0 +1,37 @@
+public class BackToSender{
+
+    public static int allowanceof(in rate){
+    
+        int amountPerParcel;
+        
+        if (number >= 50 && number <= 59){
+        
+           amountPerParcel = 200;
+              
+        }
+        
+        else if(number >= 60 && number <=69){
+        
+           amountPerParcel = 250; 
+        }
+        
+        else if(number > 50){
+        
+           amountPerParcel = 160; 
+           }
+        else{
+        
+           amountPerParcel = 500; 
+        } 
+        
+        
+        int result = number * rate + 5000;
+        
+        
+            return result;
+        }
+        
+        
+        
+    }
+

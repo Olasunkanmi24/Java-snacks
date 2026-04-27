fc3dfc57d712dbe1e4cc3aa8ff594e0d7db87b4f
Olasunkanmi24 <ayodejisodunke23@gmail.com>
2026-04-27 08:27:14 +0100

    update

---
+++ Java-snacks/wahala/BackToSenderTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.asserEquals;
+
+public class BackToSenderTest{
+
+
+    @Test 
+    public void TestToCheckIfRateIsLessThan50(){
+    
+    int number = 25;
+    
+    
+    int actual = BackToSender.allowanceOf(number);
+    
+    int expected = 9000;
+    
+    asserEquals(actual, expected);
+    
+    
+    
+    }
+    
+    @test
+    public void TesttoCheckIfRateIsGreaterThan70
+    
+    
+    int number = 80;
+    
+    int actule =BackToSender.allo
+    
+
+    }
+    
